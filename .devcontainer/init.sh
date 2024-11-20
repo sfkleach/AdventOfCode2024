60ee5151362151f0148f2ef4c94c7806b88adb4f
Stephen Leach <sfkleach@gmail.com>
2024-11-20 14:05:52 +0000

    Working devcontainer.json

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,5 +1,2 @@
 # Install poetry
 curl -sSL https://install.python-poetry.org | python3 -
-
-# Install just
-curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to $HOME/.local/bin
