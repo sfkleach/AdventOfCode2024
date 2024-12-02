d9282c2b463d263e6d3a85881f6fe5757553901f
Chris Peachey <53196139+ChrisPeachey@users.noreply.github.com>
2024-12-02 21:51:48 +0000

    part 1 day 2

--- day02/day02.py
+++ day02/day02.py
@@ -1,1 +1,32 @@
 # Common code will go here.
+def get_file_details(file_path):
+    try:
+        file_details = {}
+
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+            file_details["output"] = [line.strip() for line in lines]
+        
+        return file_details
+    except Exception as e:
+        return {"error": str(e)}
+    
+
+def is_ascending(line):
+    numbers = get_numbers(line)
+    return numbers == sorted(numbers)  
+
+def is_decending(line):
+    numbers = get_numbers(line)
+    return numbers == sorted(numbers, reverse=True)  
+
+def check_adjacent_difference(line):
+    numbers =  get_numbers(line)
+    for i in range(1, len(numbers)):
+        diff = abs(numbers[i] - numbers[i - 1]) 
+        if diff < 1 or diff > 3:  
+            return False
+    return True
+
+def get_numbers(line):
+    return list(map(int, line.split()))
--- day02/part2.py
+++ day02/part2.py
@@ -3,7 +3,26 @@ import argparse
 from pathlib import Path
 
 def run(args):
-    ...
+     data = get_file_details(args.input)
+    print(data)
+
+    ascending_or_descending = []
+    result = 0
+
+    for line in data["output"]:
+        #check if data contains increasing
+        if is_ascending(line):
+            ascending_or_descending.append(line);    
+        #check if data contains decreasing
+        elif is_decending(line): 
+            ascending_or_descending.append(line);
+            
+    for item in ascending_or_descending:
+        #check if adjacent numbers are at least 1 and at most 3
+        if check_adjacent_difference(item):
+            result = result + 1
+
+    print("Result: " + str(result))        
 
 def main():
     # Create the argument parser
