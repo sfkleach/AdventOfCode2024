da53756e5d39cdc4763fe93ba8ac7f49e8668ad6
Chris Peachey <53196139+ChrisPeachey@users.noreply.github.com>
2024-12-03 09:24:13 +0000

    Completed day 02 part 2

--- day02/day02.py
+++ day02/day02.py
@@ -29,4 +29,35 @@ def check_adjacent_difference(line):
     return True
 
 def get_numbers(line):
-    return list(map(int, line.split()))
+    return list(map(int, str(line).split()))
+
+class ReportController:
+    def __init__(self, data):
+        self.data = list(map(int, data.split())) 
+        self.reports = []
+
+        self.reports.append(data)
+
+        self.generate_reports()
+        print(self.reports)
+    
+    def generate_reports(self):
+        for i in range(len(self.data)):
+            new_report = self.Report(self.data[:i] + self.data[i+1:])
+            self.reports.append(new_report)
+    
+    def does_report_pass(self):
+        for item in self.reports:
+            if is_ascending(str(item)) or is_decending(str(item)):
+                if check_adjacent_difference(str(item)):
+                    return True               
+            else:
+                continue            
+
+    class Report:
+        def __init__(self, report):
+            self.report = report
+        def __repr__(self):
+            return f"Report({self.report})"
+        def __str__(self):
+            return " ".join(map(str, self.report))
