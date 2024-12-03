28ccf6d6b68fe06499b8e19744fc034a5c3ceccc
Chris Peachey <53196139+ChrisPeachey@users.noreply.github.com>
2024-12-03 20:55:35 +0000

    wip day 3 part 2

--- day03/day03.py
+++ day03/day03.py
@@ -1,1 +1,19 @@
 # Common code will go here.
+def get_file_details(file_path):
+    try:
+        file_details = {}
+
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+            file_details["output"] = [line.strip() for line in lines]
+        
+        return file_details
+    except Exception as e:
+        return {"error": str(e)}
+    
+def can_be_integer(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
--- day03/part1.py
+++ day03/part1.py
@@ -1,9 +1,27 @@
 from day03 import *
 import argparse
+import re
 from pathlib import Path
 
 def run(args):
-    ...
+    data = get_file_details(args.input)
+    print(data)
+
+    result = 0
+
+    # look for the pattern "mul(" + stuff + ")"
+    for item in data["output"]:
+        matches = re.findall(r"mul\(([^()]+)\)", item) #I used ChatGPT for this. I do not learn black magic like this
+        
+        print(matches)
+
+        for match in matches:
+            potential_nums = match.split(",")
+
+            if len(potential_nums) == 2 and can_be_integer(potential_nums[0]) and can_be_integer(potential_nums[1]):
+                result += int(potential_nums[0]) * int(potential_nums[1])
+            
+    print("Result: " + str(result))   
 
 def main():
     # Create the argument parser
