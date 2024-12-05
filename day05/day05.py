f36edff0c15c46e5b44a3b6bf1d37375938cb83c
Stephen Leach <sfkleach@gmail.com>
2024-12-05 23:05:02 +0000

    Day 05 Part2

--- day05/day05.py
+++ day05/day05.py
@@ -1,10 +1,13 @@
+from functools import cmp_to_key
 from pathlib import Path
+from collections import defaultdict
 
 
 class RuleSet:
 
     def __init__(self):
         self.rules = set()
+        self.dict = None
     
     def addRule(self, a, b):
         self.rules.add((a, b))
@@ -12,6 +15,43 @@ class RuleSet:
     def is_allowed(self, a, b):
         # print(f"Check: {a} < {b}")
         return (b, a) not in self.rules
+
+    def compile_dict(self):
+        self.dict = defaultdict(list)
+        for rule in self.rules:
+            (a, b) = rule
+            self.dict[a].append(b)
+
+    def next(self, a):
+        if self.dict is None:
+            self.compile_dict()
+        return self.dict[a]
+
+    def is_path(self, a, b):
+        if a == b:
+            return True
+        else:
+            for x in self.next(a):
+                # print(f"Checking: {x} -> {b}")
+                if self.is_path(x, b):
+                    return True
+            return False
+    
+    def cmp(self, a, b):
+        if self.is_path(a, b):
+            return -1
+        elif self.is_path(b, a):
+            return 1
+        else:
+            return 0
+        
+    def filter(self, pages):
+        new_rules = RuleSet()
+        p = set(pages)
+        for a, b in self.rules:
+            if a in p and b in p:
+                new_rules.addRule(a, b)
+        return new_rules
 
 class PageList:
 
@@ -28,6 +68,9 @@ class PageList:
     
     def middle(self):
         return self.pages[len(self.pages)//2]
+    
+    def sorted(self, rules: RuleSet):
+        return sorted(self.pages, key=cmp_to_key(rules.cmp))
     
 def parseInput(inputPath: Path):
     with inputPath.open('r') as file:
--- day05/part2.py
+++ day05/part2.py
@@ -2,8 +2,18 @@ from day05 import *
 import argparse
 from pathlib import Path
 
+
+
 def run(args):
-    ...
+    rules, pagelist_collection = parseInput(args.input)
+    rules.compile_dict()
+    # print(rules.dict)
+    k = 0
+    for pagelist in pagelist_collection:
+        frules = rules.filter(pagelist.pages)
+        if not pagelist.is_ordered(frules):
+            k += PageList(pagelist.sorted(frules)).middle()
+    print(f"Total: {k}")
 
 def main():
     # Create the argument parser
