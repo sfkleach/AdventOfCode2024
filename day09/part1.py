15cba72743d7100ce85b1bac502ef50f99afa228
Stephen Leach <sfkleach@gmail.com>
2024-12-09 10:56:48 +0000

    Day09 solved

--- day09/part1.py
+++ day09/part1.py
@@ -1,9 +1,64 @@
 from day09 import *
 import argparse
 from pathlib import Path
+from collections import deque
+
+class Disk:
+
+    def __init__(self, data: list[int|None]):
+        self.data = data
+        self.lo = 0
+        self.hi = len(data) - 1
+
+    def move_last(self):
+        if self.lo < self.hi:
+            self.find_first_free()
+            self.find_last_used()
+            if self.lo < self.hi:
+                self.swap()
+                return
+        raise ValueError('No more moves')
+
+
+    def swap(self):
+        self.data[self.lo] = self.data[self.hi]
+        self.data[self.hi] = None
+
+    def find_first_free(self) -> int:
+        self.lo = self.data.index(None, self.lo)
+    
+    def find_last_used(self) -> int:
+        while self.data[self.hi] is None:
+            self.hi -= 1 
+
+    def sum(self) -> int:
+        return sum(n * i for n, i in enumerate(self.data) if i is not None)
+    
+
+
+def parseInput(inputPath: Path) -> list[int|None]:
+    with open(inputPath, 'r') as file:
+        text = file.read().strip()
+    list = []
+    for n, ch in enumerate(text):
+        digit = n // 2
+        count = int(ch)
+        is_free = (n % 2) == 1
+        value = None if is_free else digit
+        list.extend(count * [value])
+    return list
+    
+        
 
 def run(args):
-    ...
+    d = Disk(parseInput(args.input))
+    try:
+        while True:
+            d.move_last()
+    except ValueError:
+        pass
+    print(d.sum())
+
 
 def main():
     # Create the argument parser
--- day09/part2.py
+++ day09/part2.py
@@ -1,9 +1,70 @@
 from day09 import *
 import argparse
 from pathlib import Path
+from typing import Generator
+
+
+class Disk:
+
+    def __init__(self, free_list: dict[int, int], used_list: dict[int, tuple[int, int]]):
+        self.free_list = free_list
+        self.used_list = used_list
+
+    def try_move_id(self, id):
+        (used_start, used_count) = self.used_list[id]
+        for free_start in sorted(self.free_list.keys()):
+            if free_start > used_start:
+                # Free store to the right will not do.
+                print('Cannot move id', id, 'to', free_start)
+                break
+            free_count = self.free_list[free_start]
+            if free_count >= used_count:
+                print(f'Can move {id}: moving {used_count} blocks to {free_start} from {used_start}')
+                self.used_list[id] = (free_start, used_count)
+                del self.free_list[free_start]
+                if free_count > used_count:
+                    self.free_list[free_start + used_count] = free_count - used_count                    
+                break
+        print('Failed to move id', id)
+
+    def rearrange(self):
+        for id in reversed(range(self.max_id() + 1)):
+            self.try_move_id(id)
+
+    def max_id(self) -> int:
+        return max(self.used_list.keys())
+
+    def factors(self) -> Generator[int, None, None]:
+        for id, (start, count) in self.used_list.items():
+            for n in range(start, start + count):
+                yield (n, id)
+
+    def sum(self) -> int:
+        return sum(n* id for n, id in self.factors())
+
+
+def parseInput(inputPath: Path) -> list[int|None]:
+    with open(inputPath, 'r') as file:
+        text = file.read().strip()
+    free_list = {}
+    used_list = {}
+    posn = 0
+    for n, ch in enumerate(text):
+        id = n // 2
+        count = int(ch)
+        is_free = (n % 2) == 1
+        if is_free:
+            free_list[posn] = count
+        else:
+            used_list[id] = (posn, count)
+        posn += count
+    return free_list, used_list
+
 
 def run(args):
-    ...
+    d = Disk(*parseInput(args.input))
+    d.rearrange()
+    print(d.sum())
 
 def main():
     # Create the argument parser
