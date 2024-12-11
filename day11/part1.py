1a86aac69d24a828b79e297022d4c6f258b8d66f
Stephen Leach <sfkleach@gmail.com>
2024-12-11 08:09:11 +0000

    Part 1 Solved

--- day11/part1.py
+++ day11/part1.py
@@ -2,8 +2,36 @@ from day11 import *
 import argparse
 from pathlib import Path
 
+def read_input(input_path: Path) -> tuple[int]:
+    with open(input_path) as f:
+        return tuple(map(int, f.read().split()))
+    
+def blink_stone(n: int):
+    if n == 0:
+        yield 1
+    else:
+        s = str(n)
+        if (len(s) % 2) == 0:
+            k = len(s) // 2
+            yield int(s[:k])
+            yield int(s[k:])
+        else:
+            yield 2024 * n
+    
+def blink_line(input_data: tuple[int]):
+    for i in input_data:
+        yield from blink_stone(i)
+
+def blink_line_n(input_data: tuple[int], n: int):
+    for _ in range(n):
+        input_data = tuple(blink_line(input_data))
+    return input_data
+
 def run(args):
-    ...
+    print("Reading the input")
+    input_data = read_input(args.input)
+    result = blink_line_n(input_data, 25)
+    print(len(result))
 
 def main():
     # Create the argument parser
