10f6e733a322b47fedf9dc2a1268b13cc9d48011
Stephen Leach <sfkleach@gmail.com>
2024-12-14 12:19:41 +0000

    Day 14 Part 1 solved

--- day14/day14.py
+++ day14/day14.py
@@ -1,1 +1,75 @@
-# Common code will go here.
+from collections import defaultdict
+from typing import Generator
+from pathlib import Path
+import re
+
+class Robot:
+
+    def __init__(self, position, velocity):
+        self.posn = position
+        self.velocity = velocity
+
+    def advance(self, width, height, steps):
+        x = self.posn[0] + steps * self.velocity[0]
+        x = x % width
+        y = self.posn[1] + steps * self.velocity[1]
+        y = y % height
+        self.posn = (x, y)
+        
+    def quadrant(self, width, height):
+        x, y = self.posn
+        w2 = width // 2
+        h2 = height // 2
+        if x == w2 or y == h2:
+            return 0
+        if x < w2:
+            if y < h2:
+                return 1
+            else:
+                return 4
+        else:
+            if y < h2:
+                return 2
+            else:
+                return 3        
+
+class Lobby:
+
+    def __init__(self, robots, width, height):
+        self.robots = tuple(robots)
+        self.width = width
+        self.height = height
+
+    def advance(self, steps):
+        for robot in self.robots:
+            robot.advance(self.width, self.height, steps)
+
+    def safety(self):
+        q = defaultdict(int)
+        for robot in self.robots:
+            q[robot.quadrant(self.width, self.height)] += 1
+        sf = 1
+        for i in range(1, 5):
+            sf *= q[i]
+        return sf
+    
+    def print(self):
+        text = [ self.width * [ 0 ] for _ in range(self.height) ]
+        print(text)
+        for robot in self.robots:
+            x = robot.posn[0]
+            y = robot.posn[1]
+            print(x,y)
+            text[y][x] += 1
+        for row in text:
+            print(''.join([str(c) if c > 0 else '.' for c in row]))
+
+            
+
+def read_input(input: Path) -> Generator[Robot, None, None]:
+    with open(input, 'r') as f:
+        for line in f:
+            # Each line contains 4 integers, separated by non-digits.
+            digits_only = re.sub(r'[^-\d]', ' ', line)
+            numbers = [int(n) for n in digits_only.split()]
+            yield Robot(numbers[:2], numbers[2:])
--- day14/part1.py
+++ day14/part1.py
@@ -1,15 +1,20 @@
-from day14 import *
+from day14 import Lobby, read_input
 import argparse
 from pathlib import Path
 
 def run(args):
-    ...
+    lobby = Lobby(read_input(args.input), args.width, args.height)
+    lobby.advance(args.count)
+    lobby.print()
+    print(lobby.safety())
 
 def main():
     # Create the argument parser
     parser = argparse.ArgumentParser(description='Process some options.')
     parser.add_argument('--input', type=Path, help='Input file path')
-    parser.add_argument('--debug', action='store_true', help='Enable debug mode')
+    parser.add_argument('--width', type=int, help='Width')
+    parser.add_argument('--height', type=int, help='Height')
+    parser.add_argument('--count', type=int, help='Count')
 
     # Parse the arguments
     args = parser.parse_args()
